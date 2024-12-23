11e1707644028dcfb713d345e8cbe88c57755857
terneo-zubr <terneo.zubr@gmail.com>
2024-12-23 18:32:13 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+import telebot
+
+TOKEN = "твой_токен_бота"
+bot = telebot.TeleBot(7625632118:AAEHZjesJZ64vQ6fQJlsmnXZJg72z5hERrc)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я Telegram-бот.")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
